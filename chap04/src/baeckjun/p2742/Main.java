e39790d63c627ea4dc63bf10fa1f1338e5deb8c3
jjeome <76250325+jjeome@users.noreply.github.com>
2022-05-23 01:22:06 +0900

    Update Main.java

--- chap04/src/baeckjun/p2742/Main.java
+++ chap04/src/baeckjun/p2742/Main.java
@@ -1,13 +1,18 @@
 package baeckjun.p2742;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		
+		Num();
 	}
 	
-	public static void () {
-		
+	public static void Num() {
+		int naNum = scan.nextInt();
+		for(int i=naNum; i>0; i--) {
+			naNum -= 1;
+			System.out.println(naNum);
+			}
 	}
-
 }
