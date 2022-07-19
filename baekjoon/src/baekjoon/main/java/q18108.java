0bfa07c998084aba64f01c4c0ef85d9625d860d3
jjeome <guswjd0151@naver.com>
2022-07-19 17:33:59 +0900

    0719

---
+++ baekjoon/src/baekjoon/main/java/q18108.java
@@ -0,0 +1,15 @@
+package baekjoon.main.java;
+
+import java.util.Scanner;
+
+public class q18108 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		int nowYear = year-543;
+		System.out.println(nowYear);
+	}
+
+}
